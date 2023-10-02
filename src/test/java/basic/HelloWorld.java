7bb7371cee85f3f1428769d77ffa8b1b578ee410
joti <joti2111@gmail.com>
2023-10-02 12:09:50 -0400

    first commit

---
+++ src/test/java/basic/HelloWorld.java
@@ -0,0 +1,14 @@
+package basic;
+
+public class HelloWorld {
+
+	public void greeting() {
+		System.out.println("Hello, Welcome to Purdue University");
+	}
+	public static void main(String[] args) {
+		HelloWorld obj = new HelloWorld();
+		obj.greeting();
+
+	}
+
+}
