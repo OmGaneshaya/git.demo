267404245754aa08343ec039896d231a05fa5271
joti <joti2111@gmail.com>
2023-10-03 11:06:31 -0400

    tuesday

--- src/test/java/basic/Maths.java
+++ src/test/java/basic/Maths.java
@@ -12,12 +12,18 @@ public class Maths {
 		System.out.println("Difference of two numbers=" + result);
 		
 	}
+	public void multi(int a, int b) {
+		int result = a*b;
+		System.out.println("multiplication of two number=" + result);
+		
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Maths obj = new Maths();
 		obj.add(5, 6);
 		obj.sub(30, 20);
+		obj.multi(3, 4);
 	}
 
 }
