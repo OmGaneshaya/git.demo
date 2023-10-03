3aba57a5bfeb9ed1ce51ac82130148b503014680
joti <joti2111@gmail.com>
2023-10-02 20:11:38 -0400

    added maths file

---
+++ src/test/java/basic/Maths.java
@@ -0,0 +1,17 @@
+package basic;
+
+public class Maths {
+	
+	public void add(int a, int b) {
+		int result = a+b;
+		System.out.println("sum of two numbers=" + result);
+		
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Maths obj = new Maths();
+		obj.add(5, 6);
+	}
+
+}
