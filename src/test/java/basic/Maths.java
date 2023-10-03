f49b4fad07b1850416327aaf0d8dd4e1690e3e55
joti <joti2111@gmail.com>
2023-10-02 22:40:26 -0400

    substraction method

--- src/test/java/basic/Maths.java
+++ src/test/java/basic/Maths.java
@@ -7,11 +7,18 @@ public class Maths {
 		System.out.println("sum of two numbers=" + result);
 		
 	}
+	public void sub(int a, int b) {
+		int result = a-b;
+		System.out.println("Difference of two numbers=" + result);
+		
+	}	
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Maths obj = new Maths();
-		obj.add(5, 6);
+		obj.add(2, 7);
+		obj.sub(35, 5);
+		
 	}
 
 }
