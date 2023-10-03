35b5465821caeb9b435861b754c399722677e448
JotiRepo <145057118+JotiRepo@users.noreply.github.com>
2023-10-02 23:28:44 -0400

    Update Maths.java

--- src/test/java/basic/Maths.java
+++ src/test/java/basic/Maths.java
@@ -13,11 +13,17 @@ public class Maths {
 		System.out.println("result of division=" + result);
 		
 	}
+	public void threenumbers(int a, int b, int c) {
+		int result = a+b-c;
+		System.out.println("result of 3 number operation=" + result);
+		
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Maths obj = new Maths();
 		obj.add(5, 6);
 		obj.divide(40,10);
+		obj.threenumber(9,4,2);
 	}
 
 }
