5a6c6f4782a47b01cc08aae1c8d6d1b59dfdf598
joti <joti2111@gmail.com>
2023-10-02 22:48:31 -0400

    edit substraction

--- src/test/java/basic/Maths.java
+++ src/test/java/basic/Maths.java
@@ -7,11 +7,17 @@ public class Maths {
 		System.out.println("sum of two numbers=" + result);
 		
 	}
+	public void sub(int a, int b) {
+		int result = a-b;
+		System.out.println("Difference of two numbers=" + result);
+		
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Maths obj = new Maths();
 		obj.add(5, 6);
+		obj.sub(30, 20);
 	}
 
 }
